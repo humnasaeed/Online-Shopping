31a03ea9e579fba163c5c99606a9f64874cae48f
humnasaeed <humna2004@gmail.com>
2024-03-07 10:21:26 -0500

    Updating product functions

---
+++ php/component.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// Creating a function to create the product on the page
+function component($itemname, $price, $img, $productid){
+    $element = "
+    
+    <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
+                <form action=\"index.php\" method=\"post\">
+                    <div class=\"card shadow\">
+                        <div>
+                            <img src=\"$img\" alt=\"Image1\" class=\"img-fluid card-img-top\">
+                        </div>
+                        <div class=\"card-body\">
+                            <h5 class=\"card-title\">$itemname</h5>
+                            <h6>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"fas fa-star\"></i>
+                                <i class=\"far fa-star\"></i>
+                            </h6>
+                            <p class=\"card-text\">
+                                Some quick example text to build on the card.
+                            </p>
+                            <h5>
+                                <small><s class=\"text-secondary\">$519</s></small>
+                                <span class=\"price\">$$price</span>
+                            </h5>
+
+                            <button type=\"submit\" class=\"btn btn-warning my-3\" name=\"add\">Add to Cart <i class=\"fas fa-shopping-cart\"></i></button>
+                             <input type='hidden' name='product_id' value='$productid'>
+                        </div>
+                    </div>
+                </form>
+            </div>
+    ";
+    echo $element;
+}
